527189aa8fece5e8b4d1662bccf43c3a043cb838
Kacper <kgrzesik17@gmail.com>
2024-06-21 17:12:36 +0200

    dodano opcje logow

--- skrypt.sh
+++ skrypt.sh
@@ -6,6 +6,14 @@ while [[ $# -gt 0 ]]; do
             date=$(date +'%Y-%m-%d')
             echo "Obecna data to: $date" 
             ;;
+        --logs)
+            date=$(date +'%Y-%m-%d')
+            for i in {1..100}
+            do
+                touch log$i.txt
+                printf "Nazwa: log$i.txt\nSkrypt: skrypt.sh\nData: $date\n" > log$i.txt
+            done
+            ;;
         *)
             echo "unknown option: $1"
             exit 1
@@ -13,5 +21,3 @@ while [[ $# -gt 0 ]]; do
     esac
     shift
 done
-
-#
