58a079b5d01b0379611a921ab2f364e04cc8d844
Kacper <kgrzesik17@gmail.com>
2024-06-21 16:53:58 +0200

    dodano wyswietlanie daty

---
+++ skrypt.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+while [[ $# -gt 0 ]]; do
+    case "$1" in
+        --date)
+            date=$(date +'%Y-%m-%d')
+            echo "Obecna data to: $date" 
+            ;;
+        *)
+            echo "unknown option: $1"
+            exit 1
+            ;;
+    esac
+    shift
+done
