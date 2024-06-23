00af85298f7856f04a6f894203cba6a087ffb780
Kacper <kgrzesik17@gmail.com>
2024-06-23 14:33:10 +0200

    dodano flage init

--- skrypt.sh
+++ skrypt.sh
@@ -2,11 +2,11 @@
 
 while [[ $# -gt 0 ]]; do
     case "$1" in
-        --date)
+        -d|--date)
             date=$(date +'%Y-%m-%d')
             echo "Obecna data to: $date" 
             ;;
-        --logs)
+        -l|--logs)
             date=$(date +'%Y-%m-%d')
             start=1
             if [[ -n "$2" ]]; then
@@ -21,11 +21,18 @@ while [[ $# -gt 0 ]]; do
                 printf "Nazwa: log$i.txt\nSkrypt: skrypt.sh\nData: $date" > log$i.txt
             done
             ;;
-        --help)
+        -i|--init)
+            git clone https://github.com/kgrzesik17/lab4-git
+            MYPATH=$(pwd)
+            export MYPATH
+            export PATH=$PATH:MYPATH
+            ;;
+        -h|--help)
             echo 'Narzedzia pracy w branzy IT - laboratorium 4'
             echo 'Opcje:'
             echo '-d --date: Wyswietla obecna date'
             echo '-l --logs [liczba]: Generuje (liczba) logow. Gdy liczba nie jest podana, domyslna wartoscia jest 100.'
+            echo '-i --init: Klonuje projekt do folderu oraz ustawia jego ściężkę w zmiennej środowiskowej PATH.'
             echo '-h --help: Wyswietla pomoc dotyczaca programu - otwiera to okno :)'
             echo 'Autor: Kacper Grzesik'
             ;;
